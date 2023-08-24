c7519da655b48710f0de6f2b08794d651a4b98d4
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-08-24 10:20:05 +0530

    updated

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ module.exports = {
 		port: process.env.PORT || 3000,
 		appName: process.env.APP_NAME || 'Courses',
 		env: process.env.NODE_ENV || 'development',
-		isProd: (process.env.NODE_ENV === 'prod'),
 		getPort: process.env.PORT || 3000,
 		getApiFolderName: process.env.API_FOLDER_NAME || 'api',
 		project_name: "Courses",
