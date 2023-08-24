63cbe4bb120537f0286db1c553ff9d0c231f6944
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-08-24 09:46:27 +0530

    dbconfig updated

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,26 +1,8 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//     const connection = await mongoose.connect('mongodb://127.0.0.1:27017/project1', {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true
-//     });
-//     console.log('Database Connected');
-// }
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
 
 module.exports = async () => {
   try {
     let db = await mongoose.connect('mongodb://' + process.env.HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE, {
-    //   auth: {
-    //     username: process.env.DB_USERNAME,
-    //     password: process.env.DB_PASSWORD
-    //   },
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
